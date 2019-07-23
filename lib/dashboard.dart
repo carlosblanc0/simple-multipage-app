2400f1ac077b555f8853605aa236c9117c90ce23
Carlos A. Blanco <cblanco@rollins.edu>
2019-07-23 14:41:10 -0400

    Create 'Dashboard.dart'

---
+++ lib/dashboard.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}

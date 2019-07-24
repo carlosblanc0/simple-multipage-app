3d11000722a80adc0a179455d926b2be0710cf96
Carlos A. Blanco <cblanco@rollins.edu>
2019-07-24 15:09:54 -0400

    Add 'TabView'

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -10,14 +10,54 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            title: Text('Dashboard'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.conversation_bubble),
+            title: Text('Support'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.profile_circled),
+            title: Text('Profile'),
+          ),
+        ],
+      ),
+      tabBuilder: (BuildContext context, int index) {
+        assert(index >= 0 && index <= 2);
+        switch (index) {
+          case 0:
+            return CupertinoTabView(
+              builder: (BuildContext context) => buildPage('Home'),
+            );
+            break;
+          case 1:
+            return CupertinoTabView(
+              builder: (BuildContext context) => buildPage('Support'),
+            );
+            break;
+          case 2:
+            return CupertinoTabView(
+              builder: (BuildContext context) => buildPage('Profile'),
+            );
+            break;
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget buildPage(String title) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
-        middle: const Text('Dashboard'),
+        middle: Text(title),
       ),
       child: Center(
-        child: CupertinoActivityIndicator(
-          radius: 32,
-        ),
+        child: CupertinoActivityIndicator(radius: 22),
       ),
     );
   }
